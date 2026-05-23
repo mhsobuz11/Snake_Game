9bb91bd2c89afef21cf9197d848fee993d8778a7
mhsobuz11 <mhsobuz11@gmail.com>
2026-05-23 06:43:48 +0600

    Implement high score tracking in Scoreboard
    Added high score functionality and reset method.

--- scoreboard.py
+++ scoreboard.py
@@ -8,20 +8,28 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data") as data:
+            self.high_score=int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,265)
         self.hideturtle()
         self.update_scoreboard()
-        # self.game_over()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data","w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
